fe1a57217be6856f72fd264049618bed4d9c6dad
Ate Frances <Ate Frances@LAPTOP-TO5O2P8R.myfiosgateway.com>
2023-05-23 22:29:24 -0400

    comment is removed, added new method

--- LearningGit/src/test/java/com/practice/git/HelloWorld.java
+++ LearningGit/src/test/java/com/practice/git/HelloWorld.java
@@ -3,8 +3,11 @@ package com.practice.git;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		//printing hello world string in the console
 		System.out.println("Hello World");
 	}
 	
+	
+	public void emptyMethod() {
+		
+	}
 }
