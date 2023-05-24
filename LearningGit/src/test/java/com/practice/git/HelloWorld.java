6aaa973ae42f5bd9aaaf4e93ec0ddb877ad8ee68
Ate Frances <Ate Frances@LAPTOP-TO5O2P8R.myfiosgateway.com>
2023-05-24 17:26:17 -0400

    updated empty method to calculate addition

--- LearningGit/src/test/java/com/practice/git/HelloWorld.java
+++ LearningGit/src/test/java/com/practice/git/HelloWorld.java
@@ -7,7 +7,9 @@ public class HelloWorld {
 	}
 	
 	
-	public void emptyMethod() {
-		
+	public void addition(int num1, int num2) {
+		int result = num1 + num2;
+		System.out.println("num1: " + num1 + ", num2: " + num2);
+		System.out.println("num1 + num2 = " + result);
 	}
 }
