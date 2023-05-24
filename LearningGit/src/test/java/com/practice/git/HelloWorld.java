3a89ca6ca6a02f09b1560a3b055c26fef49e09a8
Ate Frances <Ate Frances@LAPTOP-TO5O2P8R.myfiosgateway.com>
2023-05-23 22:26:32 -0400

    adding printing in console

--- LearningGit/src/test/java/com/practice/git/HelloWorld.java
+++ LearningGit/src/test/java/com/practice/git/HelloWorld.java
@@ -2,4 +2,9 @@ package com.practice.git;
 
 public class HelloWorld {
 
+	public static void main(String[] args) {
+		//printing hello world string in the console
+		System.out.println("Hello World");
+	}
+	
 }
