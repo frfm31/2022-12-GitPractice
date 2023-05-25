67254890b1cd47cc3fc517deda5b2b4f03d1c174
Ate Frances <Ate Frances@LAPTOP-TO5O2P8R.fios-router.home>
2023-05-24 20:29:53 -0400

    added new main method

--- LearningGit/src/test/java/com/practice/git/LoginTest.java
+++ LearningGit/src/test/java/com/practice/git/LoginTest.java
@@ -2,4 +2,8 @@ package com.practice.git;
 
 public class LoginTest {
 
+	
+	public static void main(String[] args) {
+		System.out.println("hello all, I am Johnny. I'm glad I joinedthis team !");
+	}
 }
