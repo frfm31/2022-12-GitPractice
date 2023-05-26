3907801b90fbbc516f9bc045c1cf430a2e0eadc4
Ate Frances <Ate Frances@LAPTOP-TO5O2P8R.myfiosgateway.com>
2023-05-25 22:02:54 -0400

    /

--- LearningGit/src/test/java/com/practice/git/HelloWorld.java
+++ LearningGit/src/test/java/com/practice/git/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		HelloWorld myHelloWorldobject = new HelloWorld();
+		myHelloWorldobject.addition(5,4);
 	}
 	
 	
